b6a5378a6c63d0e9d8af764570401795b5a68c10
Luis Gustavo <gustakitos@Gustavos-Macbook.local>
2019-03-30 18:34:15 -0300

    Setup production webpack build

--- src/app.js
+++ src/app.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import AppRouter from "./routers/AppRouter";
 import configureStore from "./store/configureStore";
+import "normalize.css/normalize.css";
 import "./styles/styles.scss";
+import "react-dates/lib/css/_datepicker.css";
 
 const store = configureStore();
-
+console.log("test");
 const jsx = (
   <Provider store={store}>
     <AppRouter />
